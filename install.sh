659af1560b4bb13ade17d03c6b886c34af147611
misko <misko@cs.toronto.edu>
2014-05-30 06:54:36 +0000

    adding install script

---
+++ install.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+
+# get petbot repo
+#git clone https://github.com/misko/petbot -o github ~/petbot
+git clone https://github.com/petbot/petbot-device.git -o github ~/petbot
+
+# install pip and wifi module
+wget -P /tmp https://bootstrap.pypa.io/get-pip.py
+sudo python /tmp/get-pip.py
+sudo pip install wifi
+
+
+# supervisord
+sudo apt-get -y install supervisor
+sudo ln -s ~/petbot/code/device/supervisord.conf /etc/supervisor/conf.d/petbot.conf
+
+
+# get gstreamer libraries
+
+grep -q "deb http://vontaene.de/raspbian-updates/ . main" /etc/apt/sources.list
+if [ $? -ne 0 ];
+then
+	echo "deb http://vontaene.de/raspbian-updates/ . main" | sudo tee -a /etc/apt/sources.list > /dev/null
+fi
+
+sudo apt-get update
+sudo apt-get install --force-yes -y libgstreamer1.0-0-dbg gstreamer1.0-tools libgstreamer-plugins-base1.0-0 \
+	gstreamer1.0-plugins-good gstreamer1.0-plugins-bad-dbg gstreamer1.0-omx gstreamer1.0-alsa
+
+
+# wiring PI
+git clone git://git.drogon.net/wiringPi ~/wiringPi
+cd ~/wiringPi
+sudo ./build
+cd
+
+#install mpg123
+sudo apt-get install -y mpg123
+
+#install extras
+sudo apt-get install -y vim
